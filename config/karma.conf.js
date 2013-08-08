81a8a780b7cdf0f898e896c2c5b30defa139ae3b
Evan-R <evan.gowfb@gmail.com>
2013-08-08 13:19:15 -0600

    fix config file to actually work

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -1,20 +1,27 @@
-basePath = '../';
+module.exports = function( config ){
+    config.set({
 
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'app/lib/angular/angular.js',
-  'app/lib/angular/angular-*.js',
-  'test/lib/angular/angular-mocks.js',
-  'app/js/**/*.js',
-  'test/unit/**/*.js'
-];
-
-autoWatch = true;
-
-browsers = ['Chrome'];
-
-junitReporter = {
-  outputFile: 'test_out/unit.xml',
-  suite: 'unit'
+		basePath: '../',
+		
+		frameworks: ['jasmine'],
+		
+		files: [
+		//  JASMINE,
+		//  JASMINE_ADAPTER,
+		  'app/lib/angular/angular.js',
+		  'app/lib/angular/angular-*.js',
+		  'test/lib/angular/angular-mocks.js',
+		  'app/js/**/*.js',
+		  'test/unit/**/*.js'
+		],
+		
+		autoWatch: true,
+		
+		browsers: ['Firefox'],
+		
+		junitReporter: {
+		  outputFile: 'test_out/unit.xml',
+		  suite: 'unit'
+		}
+    })
 };
